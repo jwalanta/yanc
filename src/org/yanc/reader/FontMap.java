a067ce00fb6cd3190104d8044ecfc857bb952cc7
pgautam <prasannagautam@gmail.com>
2009-01-09 08:36:39 +0545

    added some stuff for the parser.

--- src/org/yanc/reader/FontMap.java
+++ src/org/yanc/reader/FontMap.java
@@ -4,4 +4,6 @@ import java.util.Map;
 
 public interface FontMap {
 	public Map<String,String> getMap(String fontName);
+	public String toUnicode();
+	public String toFont();
 }
--- src/org/yanc/reader/XMLReaderImpl.java
+++ src/org/yanc/reader/XMLReaderImpl.java
@@ -2,6 +2,7 @@ package org.yanc.reader;
 
 import java.io.IOException;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -18,7 +19,12 @@ public class XMLReaderImpl extends DefaultHandler implements YancXMLReader {
 
 	private String FILENAME = null;
 	private XMLReader xr;
-
+	/*
+	 * This is the way I learned to do it. Looks fairly stupid but has worked so far.
+	 */
+	private boolean inFont2Unicode=false, 
+	inUnicode2Font=false,inCharacter=false,
+	inTranslation=false, inVariation=false;
 	@Override
 	public void parse() throws SAXException {
 		assert !FILENAME.equals(null); /* make sure filename is assigned */
@@ -32,7 +38,9 @@ public class XMLReaderImpl extends DefaultHandler implements YancXMLReader {
 
 	@Override
 	public void addFile(String filename) {
-		FILENAME = 
+		FILENAME = filename;
+		/* I want to load the filename using properties from a specific location
+		 * example: example /settings directory that holds the xml files. */
 	}
 
 	private void init() throws SAXException {
@@ -42,4 +50,19 @@ public class XMLReaderImpl extends DefaultHandler implements YancXMLReader {
 		xr.setErrorHandler(handler);
 	}
 
+	@Override
+	public void endElement(String uri, String localName, String name)
+			throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(uri, localName, name);
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String name,
+			Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, name, attributes);
+	}
+	
+	
 }
